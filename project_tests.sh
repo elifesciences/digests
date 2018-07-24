33e29f9a831ecffc92ef5636862c41dd6ae47e73
Sean Wiseman <seanwiseman2012@gmail.com>
2018-07-24 14:34:30 +0100

    Add db to docker-compose.ci

--- project_tests.sh
+++ project_tests.sh
@@ -3,5 +3,8 @@ set -e
 
 source venv/bin/activate
 cd app
+
+until echo > /dev/tcp/db/5432; do sleep 1; done
+
 pipenv run proofreader --target core
 pipenv run pytest --junitxml=../build/pytest.xml
