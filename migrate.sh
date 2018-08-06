0e1e8f7391a3081502d7e86817843a37aed0ff83
Giorgio Sironi <accounts@giorgiosironi.com>
2018-08-06 11:46:05 +0100

    Wait on db through Django (#20)
    * Ignore some folders
    
    * Wait on db through Django

--- migrate.sh
+++ migrate.sh
@@ -2,6 +2,7 @@
 
 source venv/bin/activate
 
-until echo > /dev/tcp/$POSTGRES_HOST/5432; do sleep 1; done
+#until echo > /dev/tcp/$POSTGRES_HOST/5432; do sleep 1; done
+until python app/manage.py inspectdb; do sleep 1; done
 
 python app/manage.py migrate
