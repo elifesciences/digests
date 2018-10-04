11e432885075b83332dab1a7029d880b73dcc21e
Giorgio Sironi <g.sironi@elifesciences.org>
2018-10-04 15:54:39 +0100

    Always serve a page it is never `None`

--- app/digests/api.py
+++ app/digests/api.py
@@ -94,12 +94,8 @@ class DigestViewSet(viewsets.ModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
 
         page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data, content_type=self.list_content_type)
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
 
     def retrieve(self, request: Request, *args, **kwargs) -> Response:
         instance = self.get_object()
